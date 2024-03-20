73c1105ce9ce118db85abdd9b3f2d46635e37bc3
kingdaryss <th.goncalves928@gmail.com>
2024-03-20 10:14:01 -0300

    ajustes buttons

--- front-end/src/app/components/button/button.component.ts
+++ front-end/src/app/components/button/button.component.ts
@@ -1,10 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() color: 'primary' | 'success' | 'warning' | 'danger' | null | 'normal' = 'normal';
+  @Input() routerLinkActive: string;
+  isActive: boolean = false;
+  constructor(private elementRef: ElementRef, private router: Router) {}
+  ngOnInit(): void {
+    this.checkParentActive();
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => this.checkParentActive());
+  }
+  checkParentActive(): void {
+    setTimeout(() => this.isActive = this.elementRef.nativeElement.classList.contains(this.routerLinkActive))
+  }
 }
